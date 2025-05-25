8acbf6864ba9a4579b9c4b95bbe9dc0bb4c2d404
Victor Miti <victormiti@umusebo.com>
2025-05-25 01:57:54 +0100

    feat: navbar

--- resources/js/app.js
+++ resources/js/app.js
@@ -110,6 +110,16 @@ document.addEventListener('alpine:init', () => {
     },
   }));
 
+  Alpine.data('mobileMenu', () => ({
+    open: false,
+    toggle() {
+      this.open = !this.open;
+    },
+    close() {
+      this.open = false;
+    },
+  }));
+
   Alpine.data('markdownEditor', () => ({
     content: '',
     activeTab: 'write',
