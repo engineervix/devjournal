a1c7cc68835550c2c1e87665bd349a2a8e7bab95
Victor Miti <victormiti@umusebo.com>
2025-05-25 20:09:36 +0100

    build: ensure test database is available for test suite

--- docker/db/init.sql
+++ docker/db/init.sql
@@ -1,2 +1,2 @@
-CREATE DATABASE devjournal;
-CREATE DATABASE test;
+SELECT 'CREATE DATABASE devjournal' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'devjournal')\gexec
+SELECT 'CREATE DATABASE test' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'test')\gexec
